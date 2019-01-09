0af866abbc072d4f1a029c58c6098655b929da80
Gautham <gautham@jnaapti.com>
2019-01-09 20:35:02 +0530

    Initial commit
    Signed-off-by: Gautham <gautham@jnaapti.com>

---
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRESTController {
+
+	@Autowired
+	ContactRepository repository;
+
+	@GetMapping("/contacts")
+	public Iterable<Contact> getContacts() {
+		return repository.findAll();
+	}
+
+}
