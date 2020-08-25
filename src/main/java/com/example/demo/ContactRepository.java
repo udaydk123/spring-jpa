da6cbeab999b06e83631b6fe4efffb30df1115da
udaydk123 <udaydk123@users.noreply.github.com>
2020-08-25 13:27:12 +0530

    Update ContactRepository.java

--- src/main/java/com/example/demo/ContactRepository.java
+++ src/main/java/com/example/demo/ContactRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
+	
+	String deleteByEmail(String email);
+	
+	List<Contact> findAll();
+	
 
 }
