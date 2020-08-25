a5f34939833f8c52604897d5964b1ad59a23be48
udaydk123 <udaydk123@users.noreply.github.com>
2020-08-25 12:11:24 +0530

    Update MyRESTController.java

--- src/main/java/com/example/demo/MyRESTController.java
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class MyRESTController {
@@ -14,5 +17,18 @@ public class MyRESTController {
 	public Iterable<Contact> getContacts() {
 		return repository.findAll();
 	}
+	
+	
+	 @PostMapping("/addContacts")
+	 Contact newContact(@RequestBody Contact newContact) {
+	    return repository.save(newContact);
+	  }
+	
+		/*
+		 * @DeleteMapping("/contacts/{email}") void deleteEmployee(@PathVariable String
+		 * email) { repository.delete(entity); }
+		 */
+	
 
 }
+
