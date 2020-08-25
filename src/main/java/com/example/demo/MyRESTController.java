22737fd4c364f3699918836e08973d70ad1423f9
udaydk123 <udaydk123@users.noreply.github.com>
2020-08-25 13:26:32 +0530

    Update MyRESTController.java

--- src/main/java/com/example/demo/MyRESTController.java
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
@@ -13,6 +19,10 @@ public class MyRESTController {
 	@Autowired
 	ContactRepository repository;
 
+	
+	
+
+	
 	@GetMapping("/contacts")
 	public Iterable<Contact> getContacts() {
 		return repository.findAll();
@@ -20,14 +30,28 @@ public class MyRESTController {
 	
 	
 	 @PostMapping("/addContacts")
-	 Contact newContact(@RequestBody Contact newContact) {
-	    return repository.save(newContact);
+	 String newContact(@RequestBody Contact newContact) {
+	     repository.save(newContact);
+	     return "created";
 	  }
 	
-		/*
-		 * @DeleteMapping("/contacts/{email}") void deleteEmployee(@PathVariable String
-		 * email) { repository.delete(entity); }
-		 */
+		
+   @DeleteMapping("/contacts/{email}")
+		  String deleteEmployee(@PathVariable String email) {
+			  repository.deleteByEmail(email);
+			  return "deleted";
+	}
+   
+   @GetMapping("/viewContacts")
+   public ModelAndView allContacts() {
+       List<Contact> list = repository.findAll();
+       ModelAndView map = new ModelAndView("contact");
+       map.addObject("contact", list);
+       return map;
+   }
+   
+   
+		 
 	
 
 }
